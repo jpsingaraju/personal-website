d4bee7423b0949de1e95741525fb15263616a88b
Jathin Pranav Singaraju <pran.singaraju@gmail.com>
2024-09-10 00:38:55 -0700

    changed tab name

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,10 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html>
+      <head>
+        <link rel="icon" href="/personal-pic.png" />
+        <title>Jathin Pranav Singaraju</title>
+      </head>
       <body className="h-full w-full bg-off-white">{children}</body>
     </html>
   );
