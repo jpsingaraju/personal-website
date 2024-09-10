1b37f2f91dd8a6b025203946b96a8a9de1839b09
Jathin Pranav Singaraju <pran.singaraju@gmail.com>
2024-09-10 00:32:56 -0700

    updated favicon.ico

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,6 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head>
-        <link rel="icon" href="/personal-pic.png" />
-      </head>
       <body className="h-full w-full bg-off-white">{children}</body>
     </html>
   );
